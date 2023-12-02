e84c9158716a927fb3287df24c8f7ac0cc07b67d
Ubuntu <ubuntu@ip-172-31-36-88.us-west-2.compute.internal>
2023-12-02 17:34:35 +0000

    initial files

---
+++ autostart_oura_justthetip.py
@@ -0,0 +1,24 @@
+from config import *
+import time
+from pt_utils import *
+from fb_utils import *
+fb=fb_utils()
+
+recent_block=fb.getKey(baseNetwork+"/recent_block")
+
+if recent_block is None or 'block' not in recent_block:
+    print("wtf, could not find last verified block.  exiting")
+    time.sleep(30)
+    exit()
+cursortext = f"{recent_block['cslot']},{recent_block['hash']}"
+command = f"/usr/local/bin/oura daemon --config /home/ubuntu/pt3_production/oura_justthetip.toml --cursor {cursortext}"
+print(command)
+print("starting Oura")
+te = runcli(command)
+if te is not None:
+    print(f"oura started successfully")
+else:
+    print(f"oura failed")
+
+print("wait 30 seconds before allowing systemctl to restart")
+time.sleep(30)
